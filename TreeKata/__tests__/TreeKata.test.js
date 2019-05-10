de099f071e66b70eef4bb568dc0a053bccfb2a55
Peter Garvin <peter.garvin@mandg.co.uk>
2019-05-10 13:19:50 +0100

    additional testing

--- TreeKata/__tests__/TreeKata.test.js
+++ TreeKata/__tests__/TreeKata.test.js
@@ -3,20 +3,21 @@ require("jest-each");
 
 describe("TreeKata", () => {
   test.each`
-    input                               | expectedResult
-    ${"TRUE"}                           | ${true}
-    ${"FALSE"}                          | ${false}
-    ${"NOT TRUE"}                       | ${false}
-    ${"NOT FALSE"}                      | ${true}
-    ${"TRUE AND FALSE"}                 | ${false}
-    ${"FALSE AND FALSE"}                | ${false}
-    ${"TRUE AND TRUE"}                  | ${true}
-    ${"TRUE OR FALSE"}                  | ${true}
-    ${"FALSE OR FALSE"}                 | ${false}
-    ${"TRUE OR TRUE"}                   | ${true}
-    ${"TRUE OR TRUE OR TRUE AND FALSE"} | ${true}
-    ${"TRUE OR FALSE AND NOT FALSE"}    | ${true}
-    ${"NOT FALSE AND TRUE AND FALSE"}   | ${false}
+    input                                                     | expectedResult
+    ${"TRUE"}                                                 | ${true}
+    ${"FALSE"}                                                | ${false}
+    ${"NOT TRUE"}                                             | ${false}
+    ${"NOT FALSE"}                                            | ${true}
+    ${"TRUE AND FALSE"}                                       | ${false}
+    ${"FALSE AND FALSE"}                                      | ${false}
+    ${"TRUE AND TRUE"}                                        | ${true}
+    ${"TRUE OR FALSE"}                                        | ${true}
+    ${"FALSE OR FALSE"}                                       | ${false}
+    ${"TRUE OR TRUE"}                                         | ${true}
+    ${"TRUE OR TRUE OR TRUE AND FALSE"}                       | ${true}
+    ${"TRUE OR FALSE AND NOT FALSE"}                          | ${true}
+    ${"NOT FALSE AND TRUE AND FALSE"}                         | ${false}
+    ${"NOT FALSE AND TRUE AND NOT TRUE OR NOT FALSE OR TRUE"} | ${true}
   `(
     "should calculate boolean of $expectedResult from $input",
     ({ input, expectedResult }) => {
