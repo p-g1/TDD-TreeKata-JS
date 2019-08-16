1257b264c3f9545f9ffcaa0f423786aae37e3364
Peter Garvin <peter.garvin@mandg.co.uk>
2019-08-16 15:04:51 +0100

    added further tests

--- TreeKata/__tests__/TreeKata.test.js
+++ TreeKata/__tests__/TreeKata.test.js
@@ -3,28 +3,31 @@ require("jest-each");
 
 describe("TreeKata", () => {
   test.each`
-    input                                                     | expectedResult
-    ${"TRUE"}                                                 | ${true}
-    ${"FALSE"}                                                | ${false}
-    ${"NOT TRUE"}                                             | ${false}
-    ${"NOT FALSE"}                                            | ${true}
-    ${"TRUE AND FALSE"}                                       | ${false}
-    ${"FALSE AND FALSE"}                                      | ${false}
-    ${"TRUE AND TRUE"}                                        | ${true}
-    ${"TRUE OR FALSE"}                                        | ${true}
-    ${"FALSE OR FALSE"}                                       | ${false}
-    ${"TRUE OR TRUE"}                                         | ${true}
-    ${"TRUE OR TRUE OR TRUE AND FALSE"}                       | ${true}
-    ${"TRUE OR FALSE AND NOT FALSE"}                          | ${true}
-    ${"NOT FALSE AND TRUE AND FALSE"}                         | ${false}
-    ${"NOT FALSE AND TRUE AND NOT TRUE OR NOT FALSE OR TRUE"} | ${true}
-    ${"(NOT FALSE)"}                                          | ${true}
-    ${"NOT (TRUE AND TRUE)"}                                  | ${false}
-    ${"(TRUE AND TRUE AND FALSE) AND FALSE"}                  | ${false}
-    ${"(TRUE AND TRUE AND TRUE) OR FALSE"}                    | ${true}
-    ${"(NOT FALSE) AND TRUE AND (TRUE OR FALSE)"}             | ${true}
-    ${"(TRUE OR TRUE OR TRUE) AND FALSE"}                     | ${false}
-    ${"(TRUE OR TRUE OR FALSE) AND FALSE"}                    | ${false}
+    input                                                                                                                  | expectedResult
+    ${"TRUE"}                                                                                                              | ${true}
+    ${"FALSE"}                                                                                                             | ${false}
+    ${"NOT TRUE"}                                                                                                          | ${false}
+    ${"NOT FALSE"}                                                                                                         | ${true}
+    ${"TRUE AND FALSE"}                                                                                                    | ${false}
+    ${"FALSE AND FALSE"}                                                                                                   | ${false}
+    ${"TRUE AND TRUE"}                                                                                                     | ${true}
+    ${"TRUE OR FALSE"}                                                                                                     | ${true}
+    ${"FALSE OR FALSE"}                                                                                                    | ${false}
+    ${"TRUE OR TRUE"}                                                                                                      | ${true}
+    ${"TRUE OR TRUE OR TRUE AND FALSE"}                                                                                    | ${true}
+    ${"TRUE OR FALSE AND NOT FALSE"}                                                                                       | ${true}
+    ${"NOT FALSE AND TRUE AND FALSE"}                                                                                      | ${false}
+    ${"NOT FALSE AND TRUE AND NOT TRUE OR NOT FALSE OR TRUE"}                                                              | ${true}
+    ${"(NOT FALSE)"}                                                                                                       | ${true}
+    ${"NOT (TRUE AND TRUE)"}                                                                                               | ${false}
+    ${"(TRUE AND TRUE AND FALSE) AND FALSE"}                                                                               | ${false}
+    ${"(TRUE AND TRUE AND TRUE) OR FALSE"}                                                                                 | ${true}
+    ${"(NOT FALSE) AND TRUE AND (TRUE OR FALSE)"}                                                                          | ${true}
+    ${"(TRUE OR TRUE OR TRUE) AND FALSE"}                                                                                  | ${false}
+    ${"(TRUE OR TRUE OR FALSE) AND FALSE"}                                                                                 | ${false}
+    ${"(NOT FALSE) AND TRUE AND FALSE OR (TRUE OR FALSE)"}                                                                 | ${true}
+    ${"(NOT FALSE) AND TRUE AND (NOT TRUE) OR (TRUE OR FALSE)"}                                                            | ${true}
+    ${"(NOT FALSE) AND TRUE AND (NOT TRUE) OR (TRUE OR FALSE) AND (NOT FALSE) AND TRUE AND (NOT TRUE) OR (TRUE OR FALSE)"} | ${true}
   `(
     "should calculate boolean of $expectedResult from $input",
     ({ input, expectedResult }) => {
